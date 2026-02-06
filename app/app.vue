f41d4ae84e08dafbc0d027ca14630c564b6a2e93
Niklas Grieger <niklas.grieger@devnik.dev>
2026-02-06 19:51:05 +0700

    typo

--- app/app.vue
+++ app/app.vue
@@ -1,47 +1,51 @@
 <script setup lang="ts">
-import { Analytics } from '@vercel/analytics/nuxt'
-import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+import { Analytics } from "@vercel/analytics/nuxt";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
-const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
+const color = computed(() =>
+  colorMode.value === "dark" ? "#020618" : "white",
+);
 
 useHead({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color }
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
   ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
 useSeoMeta({
-  titleTemplate: '%s - Niklas Grieger'
-})
-defineOgImage({ url: '/profile.jpg' })
+  titleTemplate: "%s - Niklas Grieger",
+});
+defineOgImage({ url: "/profile.jpg" });
 
 const [{ data: navigation }, { data: files }] = await Promise.all([
-  useAsyncData('navigation', () => {
-    return Promise.all([
-      queryCollectionNavigation('blog')
-    ])
-  }, {
-    transform: data => data.flat()
-  }),
-  useLazyAsyncData('search', () => {
-    return Promise.all([
-      queryCollectionSearchSections('blog')
-    ])
-  }, {
-    server: false,
-    transform: data => data.flat()
-  })
-])
+  useAsyncData(
+    "navigation",
+    () => {
+      return Promise.all([queryCollectionNavigation("blog")]);
+    },
+    {
+      transform: (data) => data.flat(),
+    },
+  ),
+  useLazyAsyncData(
+    "search",
+    () => {
+      return Promise.all([queryCollectionSearchSections("blog")]);
+    },
+    {
+      server: false,
+      transform: (data) => data.flat(),
+    },
+  ),
+]);
 </script>
 
 <template>
@@ -64,7 +68,7 @@ const [{ data: navigation }, { data: files }] = await Promise.all([
 
     <a
       target="_blank"
-      href="https://wa.me/+4915679009238?text=Hello,%20I'm%20found%20you%20on%20your%20Website%20and%20I%20may%20need%20an%20developer%20for%20my%20business"
+      href="https://wa.me/+4915679009238?text=Hello,%20I%20found%20you%20on%20your%20Website%20and%20I%20may%20need%20an%20developer%20for%20my%20business"
     >
       <UIcon
         class="fixed bottom-4 right-4 size-10"
